5617ef4bc754fa94e6ae3fba47e7edede52fc0a9
khaoularj <khaoula.raja1@gmail.com>
2023-11-08 10:17:23 -0800

    task 5: create class FileStorage in models/engine/file_storage.py, update models/__init__.py to create a unique FileStorage instance for the application, Update models/base_model.py to link BaseModel to FileStorage by using the variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Define class called BaseModel"""
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """this is the BaseModel that defines all
@@ -27,11 +27,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """adding the public methods that updates the public
         instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """adding the public method that returns
---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a new class called FileStorage"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """this is the FileStorage that serializes instances to a
+    JSON file and deserializes JSON file to instance"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """adding public instance methods that returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """adding public instance methods that  sets in
+        __objects the obj with key <obj class name>.id"""
+        obj_class_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(obj_class_name, obj.id)] = obj
+
+    def save(self):
+        """adding public instance methods that serializes
+        __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            data_file = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+            json.dump(data_file, f)
+
+    def reload(self):
+        """adding public instance methods that deserializes
+        the JSON file to __objects (only if the JSON file (__file_path) exists
+        otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
+        try:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+                data_file = json.load(f)
+                for k, v in data_file.items():
+                    class_name, obj_id = k.split(".")
+                    obj = globals()[class_name](**v)
+                    FileStorage.__objects[k] = obj
+        except FileNotFoundError:
+            pass
